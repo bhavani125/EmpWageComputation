12d6f28a730f2a5a1fbab34913bdcfdedd028024
bhavani125 <girinenibhavani12@gmil.com>
2021-06-16 12:46:02 +0530

    added code for monthly employe wages

--- EmpWageComputation.java
+++ EmpWageComputation.java
@@ -3,28 +3,35 @@ public class EmpWageComputation
   public static void main(String[] args)
      {
 	 System.out.println("Welcome to employee Wage Computation Program");
-	 
- int isFullTime=1;
+      int isFullTime=1;
      int isPartTime=2;
-     int DailyWage;
      int WagePerHr=20;
      int WorkingHrs;
+     int DaysInMonth=20;
+     int MonthlyTotalWage=0;
+     int DailyWageArray[ ] = new int[20];
+     for (int i=0; i<DaysInMonth; i++)
+     {
      int empcheck = (int)(Math.floor(Math.random() * 10)) % 3;
      switch (empcheck)
      {
        case 1:
-           System.out.println("Employee is present FullTime");
-           WorkingHrs=8;
+            WorkingHrs=8;
            break;
        case 2:
-          System.out.println("Employee is present for PartTime");
           WorkingHrs=4;
           break;
        default:
-        System.out.println("Employee is Absent");
         WorkingHrs=0;
       }
-     DailyWage=WorkingHrs * WagePerHr;
-     System.out.println("Daily wage of employee is " + DailyWage);
-    }
+     DailyWageArray[i]=WorkingHrs*WagePerHr;
+     }
+     for (int j=0; j<DaysInMonth; j++)
+     {
+       int day = j+1;
+     System.out.println("Day" +day+ "wage is" + DailyWageArray[j]);
+     MonthlyTotalWage = MonthlyTotalWage + DailyWageArray[j];
+     }
+    System.out.println("\nMonthly wage is" +  MonthlyTotalWage );
+  }
 }
