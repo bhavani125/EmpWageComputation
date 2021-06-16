58eb16c4bb509681fe7d8df3f5e5891a061a8f9b
bhavani125 <girinenibhavani12@gmil.com>
2021-06-16 12:22:47 +0530

    added method for employee atttendence

--- EmpWageComputation.java
+++ EmpWageComputation.java
@@ -2,6 +2,14 @@ public class EmpWageComputation
 {
   public static void main(String[] args)
      {
-       System.out.println("Welcome to Employee Wage Computation Program");
-   }
+	 System.out.println("Welcome to employee Wage Computation Program");
+     int isFullTime=1;
+     double empcheck = Math.floor(Math.random() * 10) % 2;
+     if(empcheck == isFullTime)
+     {
+       System.out.println("Employee is present");
+     }
+     else
+        System.out.println("Employee is Absent");
+	 }
 }
