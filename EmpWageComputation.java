a71c0b10ff0429ae6c8d6254e420ef199814ac42
bhavani125 <girinenibhavani12@gmil.com>
2021-06-16 12:42:35 +0530

    added code using Switch case

--- EmpWageComputation.java
+++ EmpWageComputation.java
@@ -3,28 +3,28 @@ public class EmpWageComputation
   public static void main(String[] args)
      {
 	 System.out.println("Welcome to employee Wage Computation Program");
-     int isFullTime=1;
+	 
+ int isFullTime=1;
      int isPartTime=2;
      int DailyWage;
      int WagePerHr=20;
      int WorkingHrs;
-     double empcheck = Math.floor(Math.random() * 10) % 3;
-     if(empcheck == isFullTime)
+     int empcheck = (int)(Math.floor(Math.random() * 10)) % 3;
+     switch (empcheck)
      {
-       System.out.println("Employee is present");
-       WorkingHrs=8;
-     }
-     else if(empcheck == isPartTime)
-     {
-        System.out.println("Employee is present for PartTime");
-        WorkingHrs=4;
-     }
-     else
-      {
+       case 1:
+           System.out.println("Employee is present FullTime");
+           WorkingHrs=8;
+           break;
+       case 2:
+          System.out.println("Employee is present for PartTime");
+          WorkingHrs=4;
+          break;
+       default:
         System.out.println("Employee is Absent");
         WorkingHrs=0;
       }
      DailyWage=WorkingHrs * WagePerHr;
      System.out.println("Daily wage of employee is " + DailyWage);
-	 }
+    }
 }
