2f4cddcb041dc3de5576e225e72b3d63de7fb724
bhavani125 <girinenibhavani12@gmil.com>
2021-06-22 16:21:10 +0530

    Added: code of emp wage computation

--- EmpWageComputation.java
+++ EmpWageComputation.java
@@ -1,40 +1,39 @@
-public class EmpWageComputation
-{
-  public static void main(String[] args)
-     {
-	 System.out.println("Welcome to employee Wage Computation Program");
-     int isFullTime=1;
-     int isPartTime=2;
-     int WagePerHr=20;
-     int WorkingHrs;
-     int TotalWorkingHrs=0;
-     int DaysInMonth=20;
-     int MaxWorkingHrs=100;
-     int MonthlyTotalWage=0;
-     int DailyWageArray[ ] = new int[20];
-     for (int i=0; i<DaysInMonth &&  TotalWorkingHrs< MaxWorkingHrs; i++)
-     {
-     int empcheck = (int)(Math.floor(Math.random() * 10)) % 3;
-     switch (empcheck)
-     {
-       case 1:
-            WorkingHrs=8;
-           break;
-       case 2:
-          WorkingHrs=4;
-          break;
-       default:
-        WorkingHrs=0;
-      }
-      TotalWorkingHrs += WorkingHrs;
-     DailyWageArray[i]=WorkingHrs*WagePerHr;
-     }
-     for (int j=0; j<DaysInMonth; j++)
-     {
-       int day = j+1;
-     System.out.println("Day" +day+ "wage is" + DailyWageArray[j]);
-     MonthlyTotalWage = MonthlyTotalWage + DailyWageArray[j];
-     }
-    System.out.println("\nMonthly wage is" +  MonthlyTotalWage );
-  }
+
+public class EmpWageComputation {
+    //constant variables
+    public static int isPresent = 1, isPartTime = 2, wagePerHr = 20, maxWorkingHours = 100, daysInMonth = 20;
+    public static int dailyWageArray[] = new int[20];
+    public static int workingHrs, totalWorkingHrs = 0, monthlyWage = 0;
+
+
+    //creating main method
+    public static void main(String[] args) {
+        System.out.println("Welcome to Employee Wage Computation problem");
+        //creating object
+        EmpWageComputation wage= new EmpWageComputation();
+        //calling the method  monthlyWage to the object we created
+        wage.monthlyWage();
+    }
+
+    // creating a method to calculate monthly wages
+    void monthlyWage() {
+        for (int i=0; i<daysInMonth&& totalWorkingHrs<maxWorkingHours; i++) {
+            int empAttendance= (int)(Math.floor(Math.random() * 10)) % 3;
+            switch (empAttendance) {
+                case 1 -> workingHrs = 8;
+                case 2 -> workingHrs = 4;
+                default -> workingHrs = 0;
+            }
+            totalWorkingHrs+= workingHrs;
+            dailyWageArray[i] = workingHrs * wagePerHr;
+        }
+        for (int j=0; j<daysInMonth; j++) {
+            int day = j+1;
+            System.out.println(" employee Day " + day + " wage is " + dailyWageArray[j]);
+            monthlyWage = monthlyWage + dailyWageArray[j];
+        }
+        System.out.println("\nMonthly wage of an employee is " + monthlyWage);
+    }
+
 }
+
